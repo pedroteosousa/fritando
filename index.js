5d89b526f8e16f34f789e22eaca1a77205a96f04
pedroteosousa <pedroteosousa@gmail.com>
2019-04-23 15:47:00 -0300

    Add argument parsing

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
+#!/usr/bin/env node
+
 const codeforces = require("cf-api-wrapper")
+const program = require("commander")
+const chalk = require('chalk')
 
-codeforces.contest.list().then((data) => data.result.reduce((contest_ids, contest) => {
-    if (contest.relativeTimeSeconds > 0)
-        return [...contest_ids, contest.id]
-    return contest_ids
-}, [])).then(ids => {
-    const num_handles = process.argv.length
-    const promises = []
-    for (var i = 2; i < num_handles; i++) {
-        promises.push(codeforces.user.status({handle: process.argv[i]})
-            .then(data => data.result.reduce((contest_ids, submission) => [...contest_ids, submission.contestId], [])))
-    }
-    Promise.all(promises).then(users_bad_contests => {
-        var bad_contests = []
-        users_bad_contests.forEach(ids => {
-            bad_contests = bad_contests.concat(ids)
+program
+    .version('0.1.0')
+    .usage('[options] <file ...>')
+    .option('-s, --size <n>', 'amount of contests to consider', parseInt)
+    .option('-h, --handles <items>', 'list of user handles', (list) => list.split(','))
+    .parse(process.argv)
+
+const getURL = id => {
+    return 'https://codeforces.com/contest/' + id
+}
+
+const getGoodContests = (size, handles) => {
+    return codeforces.contest.list().then((data) => data.result.reduce((contest_ids, contest) => {
+        if (contest.relativeTimeSeconds > 0)
+            return [...contest_ids, {
+                name: contest.name,
+                id: contest.id,
+                url: getURL(contest.id),
+            }]
+        return contest_ids
+    }, [])).then(ids => {
+        const promises = []
+        handles.forEach(handle => {
+            promises.push(codeforces.user.status({handle})
+                .then(data => data.result.reduce((contest_ids, submission) => [...contest_ids, submission.contestId], [])))
         })
-        bad_contests = [...new Set(bad_contests)]
-        const good_contests = ids.filter(id => bad_contests.indexOf(id) === -1)
-        //
+        return Promise.all(promises).then(users_bad_contests => {
+            var bad_contests = []
+            users_bad_contests.forEach(ids => {
+                bad_contests = bad_contests.concat(ids)
+            })
+            bad_contests = [...new Set(bad_contests)]
+            return ids.filter(contest => bad_contests.indexOf(contest.id) === -1).slice(0, size)
+        })
     })
-})
+}
 
+getGoodContests(program.size, program.handles).then(contests => contests.forEach(({name, url}) => {
+    console.log(chalk.blue(name) + ": " + url)
+}))
