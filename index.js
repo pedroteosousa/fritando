a1f04f2e7b407d6425bb0f3c4185d0e97cf8ba1c
Pedro Sousa <pedroteosousa@gmail.com>
2019-06-17 14:42:08 -0300

    Fix problems when giving a bunch of users
    Also, add a progress bar

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
+const _cliProgress = require('cli-progress')
 const codeforces = require("cf-api-wrapper")
 const program = require("commander")
 const chalk = require('chalk')
+const promiseRetry = require('promise-retry')
 
 program
     .version('0.1.0')
@@ -15,8 +17,27 @@ const getURL = id => {
     return 'https://codeforces.com/contest/' + id
 }
 
+const retryCall = (call) => {
+    return promiseRetry((retry, number) => {
+        return new Promise((resolve, reject) => {
+            call().then(data => {
+                if (data.status === undefined) {
+                    reject()
+                } else {
+                    resolve(data)
+                }
+            })
+        }).catch(retry)
+    })
+}
+
 const getGoodContests = (size, handles) => {
-    return codeforces.contest.list().then((data) => data.result.reduce((contest_ids, contest) => {
+    const bar = new _cliProgress.Bar({
+        stopOnComplete: true,
+        format: '[{bar}] {percentage}% | {value}/{total}',
+    })
+    bar.start(handles.length + 1, 0)
+    return retryCall(codeforces.contest.list).then(data => data.result.reduce((contest_ids, contest) => {
         if (contest.relativeTimeSeconds > 0)
             return [...contest_ids, {
                 name: contest.name,
@@ -25,10 +46,13 @@ const getGoodContests = (size, handles) => {
             }]
         return contest_ids
     }, [])).then(ids => {
+        bar.increment()
         const promises = []
         handles.forEach(handle => {
-            promises.push(codeforces.user.status({handle})
-                .then(data => data.result.reduce((contest_ids, submission) => [...contest_ids, submission.contestId], [])))
+            promises.push(retryCall(() => codeforces.user.status({handle})).then(data => {
+                bar.increment()
+                return data.result.reduce((contest_ids, submission) => [...contest_ids, submission.contestId], [])
+            }))
         })
         return Promise.all(promises).then(users_bad_contests => {
             var bad_contests = []
@@ -36,11 +60,13 @@ const getGoodContests = (size, handles) => {
                 bad_contests = bad_contests.concat(ids)
             })
             bad_contests = [...new Set(bad_contests)]
+            
             return ids.filter(contest => bad_contests.indexOf(contest.id) === -1).slice(0, size)
         })
     })
 }
 
 getGoodContests(program.size, program.handles).then(contests => contests.forEach(({name, url}) => {
-    console.log(chalk.blue(name) + ": " + url)
+   console.log(chalk.blue(name) + ": " + url)
 }))
+
